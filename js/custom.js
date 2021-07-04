ad20cb2887c60c20fc1804e5e443c94f667f67b2
Tausif_Iqbal <tausifiqbalti21@gmail.com>
2021-07-04 15:36:51 +0530

    added mail service

---
+++ js/custom.js
@@ -0,0 +1,29 @@
+function sendMail(params) {
+    console.log("sending mail ...");
+    var tempParams = {
+
+
+        message: document.getElementById("msg").value,
+        from_name: document.getElementById("fromName").value,
+        E_mail: document.getElementById("Email").value,
+    };
+    console.log(document.getElementById("fromName").value);
+    console.log(document.getElementById("msg").value);
+    console.log(document.getElementById("Email").value);
+    emailjs.send('service_tuwkfzp', 'template_ntoztbf', tempParams)
+        .then(function (res) {
+            if (res.status != 200) {
+                alert("ERROR")
+            } else {
+                if (res.response = "OK") {
+                    alert("SUCCESSFUL! ");
+                } else {
+                    alert("FAILURE!");
+                }
+            }
+            console.log("success", res.status);
+        });
+
+
+
+}
